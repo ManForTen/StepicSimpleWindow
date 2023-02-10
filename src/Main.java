3a83aa316be9d196fb290ca18a0e6fe0c8f4f701
Artem Petrikov <manforten@tandex.ru>
2023-02-10 22:21:53 +0300

    New changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Окно");
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = 500, height = 500;
+        frame.setBounds(dim.width / 2 - width / 2, dim.height / 2 - height / 2, width, height);
+        JPanel panel = new JPanel ();
+        panel.setFocusable(true);
+        frame.add(panel);
+        panel.addKeyListener(new KeyAdapter() {
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode()==KeyEvent.VK_SPACE) // Если нажат пробел
+                    new JOptionPane().showMessageDialog(panel,"Артём","Имя", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        frame.setVisible(true);
     }
 }
